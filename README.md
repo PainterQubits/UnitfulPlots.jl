cede2f8402ca65f814ff3dc7bd638e90cc9be4a6
Andrew Keller <andrew.keller.09@gmail.com>
2016-09-16 09:36:14 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # UnitfulPlots
+
+Enables automatic labeling of units on axes of plots generated with
+[Plots.jl](http://github.com/tbreloff/Plots.jl). The unitful quantities are
+created in the first place using
+[Unitful.jl](http://github.com/ajkeller34/Unitful.jl).
+
+To use, simply type `using UnitfulPlots` in Julia 0.5 or higher
+and plot arrays of quantities as you usually would.
+
+This package is experimental, please report unexpected behavior.
--- src/UnitfulPlots.jl
+++ src/UnitfulPlots.jl
@@ -1,5 +1,80 @@
 module UnitfulPlots
+using Unitful
+importall RecipesBase
 
-# package code goes here
+function unit_formatter(T, num)
+    string(num)*string(unit(zero(T)))
+end
 
-end # module
+# 2D plots (1 recipe for shorthand form)
+@recipe function f{T<:Quantity}(val::AbstractVector{T})
+    yformatter := x->unit_formatter(T,x)
+    ustrip(val)
+end
+
+# 2D plots (2^4-1 user recipes)
+@recipe function f{S<:Quantity, T<:Quantity}(val::AbstractVector{S}, val2::AbstractVector{T})
+    xformatter := x->unit_formatter(S,x)
+    yformatter := x->unit_formatter(T,x)
+    ustrip(val), ustrip(val2)
+end
+
+@recipe function f{S<:Number, T<:Quantity}(val::AbstractVector{S}, val2::AbstractVector{T})
+    yformatter := x->unit_formatter(T,x)
+    val, ustrip(val2)
+end
+
+@recipe function f{S<:Number, T<:Quantity}(val::AbstractVector{T}, val2::AbstractVector{S})
+    xformatter := x->unit_formatter(T,x)
+    ustrip(val), val2
+end
+
+# 3D plots (2^8-1 user recipes)
+@recipe function f{S<:Quantity, T<:Quantity, U<:Quantity}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    xformatter := x->unit_formatter(S,x)
+    yformatter := x->unit_formatter(T,x)
+    zformatter := x->unit_formatter(U,x)
+    ustrip(val), ustrip(val2), ustrip(val3)
+end
+
+@recipe function f{S<:Quantity, T<:Quantity, U<:Number}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    xformatter := x->unit_formatter(S,x)
+    yformatter := x->unit_formatter(T,x)
+    ustrip(val), ustrip(val2), val3
+end
+
+@recipe function f{S<:Quantity, T<:Number, U<:Quantity}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    xformatter := x->unit_formatter(S,x)
+    zformatter := x->unit_formatter(U,x)
+    ustrip(val), val2, ustrip(val3)
+end
+
+@recipe function f{S<:Number, T<:Quantity, U<:Quantity}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    yformatter := x->unit_formatter(T,x)
+    zformatter := x->unit_formatter(U,x)
+    val, ustrip(val2), ustrip(val3)
+end
+
+@recipe function f{S<:Quantity, T<:Number, U<:Number}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    xformatter := x->unit_formatter(S,x)
+    ustrip(val), val2, val3
+end
+
+@recipe function f{S<:Number, T<:Quantity, U<:Number}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    yformatter := x->unit_formatter(T,x)
+    val, ustrip(val2), val3
+end
+
+@recipe function f{S<:Number, T<:Number, U<:Quantity}(
+    val::AbstractVector{S}, val2::AbstractVector{T}, val3::AbstractMatrix{U})
+    zformatter := x->unit_formatter(U,x)
+    val, val2, ustrip(val3)
+end
+
+end
